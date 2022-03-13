13e2e8d4c81ef2b8e886984390478b30e5f3ea6c
Charlie Lee <clee@tuta.io>
2022-03-12 16:37:27 -0800

    add CRUD operations for links

--- Services/UserLinkService.cs
+++ Services/UserLinkService.cs
@@ -15,9 +15,35 @@ namespace cSharpAuth.Services
             client = new swaggerClient("https://couchclient.leenet.link",_httpClient);
         }
 
-        public async Task<IEnumerable<UserLink>> GetUserLinksAsync(string search)
+        public async Task<IEnumerable<UserLink>> List(string search)
         {
             return await client.UserlinksAsync(search, null, null);
+        }
+        public async Task<UserLink> Get(string id)
+        {
+            Guid g = new Guid(id);
+            return await client.UserLinkGetByIdAsync(g);
+        }
+        public async Task Put(UserLink link)
+        {
+            UserLinkUpdateRequestCommand cmd = new UserLinkUpdateRequestCommand();
+            cmd.Content = link.Content;
+            cmd.Href = link.Href;
+            cmd.Pid = link.Pid;
+            cmd.Target = link.Target;
+            await client.Userlink3Async(cmd);
+        }
+        public async Task Post(UserLink link)
+        {
+            UserLinkCreateRequestCommand cmd = new UserLinkCreateRequestCommand();
+            cmd.Content = link.Content;
+            cmd.Href = link.Href;
+            cmd.Target = link.Target;
+            await client.Userlink2Async(cmd);
+        }
+        public async Task Delete(Guid Pid)
+        {
+            await client.UserlinkAsync(Pid);
         }
     }
 }
